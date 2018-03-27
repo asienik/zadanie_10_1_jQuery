8a048f82e88e174dd963c4fd7911338f37fece8e
Joanna Fengler <asiapobuta@wp.pl>
2018-03-28 01:45:01 +0200

    layout corrections

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,10 @@ var span = $('span');
 var paragraphs = $('p');
 
 	paragraphs.each(function(index, element) {
-	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-	$(element).append(button);
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+		$(element).append(button);
 	});
 
 	$('button').click(function(){
-	alert($(this).attr('data-tmp'));
+		alert($(this).attr('data-tmp'));
 	});
